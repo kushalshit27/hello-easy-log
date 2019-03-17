88981560975e8aea7041869a2235840055b40c5e
kushalshit2 <kushal.shit@geotechinfo.net>
2019-03-17 13:58:45 +0530

    version update

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ module.exports.log = (code, msg, data) => {
 module.exports.welcome= (name) => {
     return (`Hello ${name}`)
 }
+
+module.exports.version= () => {
+    return (`version 0.1.4`)
+}
