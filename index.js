f72be91dcb111acfed296a89829b23bb8f1e3a31
Kushal <43465488+kushalshit27@users.noreply.github.com>
2019-03-17 12:22:03 +0530

    index
    init

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports.log = (code, msg, data) => {
+        if($code){
+            code = null;
+        }
+        if($msg){
+            msg = null;
+        }
+        if($data){
+            data = null;
+        }
+        return (`${code}, ${msg}, ${data}`)
+}
+
+module.exports.welcome= (name) => {
+    console.log(`Hello ${name}`)
+}
